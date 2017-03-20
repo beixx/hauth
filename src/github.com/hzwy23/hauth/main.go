5f1377c90402199891020b8fd35a607e29f3a907
hzwy23 <hzwy23@163.com>
2017-03-20 13:51:09 +0800

    update

--- src/github.com/hzwy23/hauth/main.go
+++ src/github.com/hzwy23/hauth/main.go
@@ -1,11 +1,6 @@
 package main
 
 import (
-
-	_ "github.com/hzwy23/mas/ca"
-	_ "github.com/hzwy23/mas/ftp"
-	_ "github.com/hzwy23/mas/common"
-
 	"github.com/astaxie/beego"
 
 	"github.com/astaxie/beego/context"
@@ -13,20 +8,19 @@ import (
 	"github.com/hzwy23/hauth/service"
 )
 
-
 func main() {
 
 	// 开启消息，
 	// 将80端口的请求，重定向到443上
 	go service.RedictToHtpps()
 
-	beego.InsertFilter("/*",beego.FinishRouter, func(ctx *context.Context) {
+	beego.InsertFilter("/*", beego.FinishRouter, func(ctx *context.Context) {
 		go service.WriteHandleLogs(ctx)
-	},false)
+	}, false)
 
 	beego.InsertFilter("/v1/*", beego.BeforeRouter, func(ctx *context.Context) {
 		service.CheckJWT(ctx)
-	},false)
+	}, false)
 
 	beego.Run()
 }
